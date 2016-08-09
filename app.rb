c037d4c3ef63193fa58030c8d81d1fa9b3466543
Derick Quartey <niikwartey1@gmail.com>
2016-08-09 13:41:41 -0400

    added form with nested params

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require './environment'
+require_relative 'app/models/pirate'
+require_relative 'app/models/ship'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(
+        params["pirate"]["name"],
+        params["pirate"]["height"],
+        params["pirate"]["weight"]
+      )
+
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      end
+
+      @pirate_ships = Ship.all
+
+      erb :"pirates/show"
+    end
 
   end
 end
+ 
